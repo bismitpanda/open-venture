0636919959ece3f43b55fe60a455f6950feb1881
Bismit Panda <adnaptimsib333@gmail.com>
2025-02-24 20:22:18 +0530

    fix: added avatar stack and metrics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import TestimonialSection from "@/components/testimonial";
 import ServiceCard from "@/components/service-card";
 import { PartnerSection } from "@/components/partner-button";
 import Footer from "@/components/footer";
+import { AvatarStack } from "@/components/avatar-stack";
 
 export default function Page() {
   const stats = [
@@ -104,11 +105,33 @@ export default function Page() {
       </section>
 
       <section className="bg-blue-50 py-16">
+        <div className="max-w-7xl mx-auto px-4 py-8">
+          <div className="flex flex-row max-w-fit mx-auto space-x-10">
+            <span className="text-5xl font-bold bg-gradient-to-br from-[#1454A1] to-[#AC049B] text-transparent bg-clip-text">
+              10,000+
+            </span>
+            <AvatarStack
+              avatars={[
+                "/avatars/avatar-1.jpg",
+                "/avatars/avatar-2.jpg",
+                "/avatars/avatar-3.jpg",
+                "/avatars/avatar-4.jpg",
+                "/avatars/avatar-5.jpg",
+                "/avatars/avatar-6.jpg",
+              ]}
+              maxAvatarsAmount={6}
+              className="items-center"
+            />
+          </div>
+          <div className="text-3xl font-light my-4 mx-auto max-w-fit">
+            Immigrants satisfied with our services since 2018.
+          </div>
+        </div>
         <div className="max-w-7xl mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 text-center">
             {stats.map((stat, index) => (
               <div key={index}>
-                <div className="text-2xl font-bold text-blue-600">
+                <div className="text-4xl font-bold text-blue-600">
                   {stat.number}
                 </div>
                 <p className="text-sm text-gray-600">{stat.description}</p>
