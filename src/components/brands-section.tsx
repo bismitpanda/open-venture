c22e9bab77327ec27ba2467a7ecc64eedbfbcd6c
Bismit Panda <adnaptimsib333@gmail.com>
2025-03-20 18:39:09 +0530

    edit

--- src/components/brands-section.tsx
+++ src/components/brands-section.tsx
@@ -8,19 +8,20 @@ export function PartnerSection() {
     "unshackled",
     "shout-start",
     "open-tax-advisors",
-  ];
+    "open-axle",
+  ] as const;
 
   return (
     <section className="py-16 mb-10">
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="mx-auto px-24">
         <h2 className="text-3xl font-medium text-center mb-8">Our Brands</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 items-center justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8 items-center justify-items-center">
           {brands.map((brand) => (
             <div key={brand}>
               <Image
-                src={`/brands/${brand.toLowerCase()}.jpg`}
+                src={`/brands/${brand}.png`}
                 alt={brand}
-                height={240}
+                height={200}
                 width={600}
               />
             </div>
